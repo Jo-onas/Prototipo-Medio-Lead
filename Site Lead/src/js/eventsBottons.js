a799e12b450cd74989ade6d334df782fc5a0a107
Daniel Fernando <87868222+danielfernandoaraujo@users.noreply.github.com>
2021-11-06 21:45:04 -0300

    Add files via upload

---
+++ Site Lead/src/js/eventsBottons.js
@@ -0,0 +1,20 @@
+
+let programacao = document.getElementById('btn-Programacao').addEventListener('click', () => {
+    title.innerHTML= 'PROGRAMAÇÃO' //Altere aqui o titulo (sempre dentro das 'aspas')
+    subtitle.innerHTML= 'PROGRAMAÇÃO BLA BLA BLA' //Altere aqui a descrição
+    img.innerHTML = '<img src="images/imagem.jpg" alt="">' //Altere aqui o caminho da imagem
+})
+let marketing = document.getElementById('btn-Marketing').addEventListener('click', () => {
+    title.innerHTML= 'MARKETING' //Altere aqui o titulo (sempre dentro das 'aspas')
+    subtitle.innerHTML= 'MARKETING BLA BLA BLA'  //Altere aqui a descrição
+    img.innerHTML = '<img src="images/instagram.png" alt="">' //Altere aqui o caminho da imagem
+})
+let producao = document.getElementById('btn-Producao').addEventListener('click', () => {
+    title.innerHTML= 'PRODUÇÃO' //Altere aqui o titulo (sempre dentro das 'aspas')
+    subtitle.innerHTML= 'PRODUÇÃO BLA BLA BLA' //Altere aqui a descrição
+    img.innerHTML = '<img src="images/alura.png" alt="">' //Altere aqui o caminho da imagem
+})
+
+let title =  document.getElementById('title-botton')
+let subtitle =  document.getElementById('subtitle-botton')
+let img = document.getElementById('imge')
