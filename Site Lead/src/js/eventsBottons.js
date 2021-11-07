2baa48b51c08b9d0150f200bb810627db2ffc07b
Daniel Fernando <87868222+danielfernandoaraujo@users.noreply.github.com>
2021-11-06 21:45:50 -0300

    eventos adicionados aos botões

--- Site Lead/src/js/eventsBottons.js
+++ Site Lead/src/js/eventsBottons.js
@@ -3,18 +3,23 @@ let programacao = document.getElementById('btn-Programacao').addEventListener('c
     title.innerHTML= 'PROGRAMAÇÃO' //Altere aqui o titulo (sempre dentro das 'aspas')
     subtitle.innerHTML= 'PROGRAMAÇÃO BLA BLA BLA' //Altere aqui a descrição
     img.innerHTML = '<img src="images/imagem.jpg" alt="">' //Altere aqui o caminho da imagem
+    //imagem só pra ver que tá funcionando
 })
 let marketing = document.getElementById('btn-Marketing').addEventListener('click', () => {
     title.innerHTML= 'MARKETING' //Altere aqui o titulo (sempre dentro das 'aspas')
     subtitle.innerHTML= 'MARKETING BLA BLA BLA'  //Altere aqui a descrição
     img.innerHTML = '<img src="images/instagram.png" alt="">' //Altere aqui o caminho da imagem
+    //imagem só pra ver que tá funcionando
 })
 let producao = document.getElementById('btn-Producao').addEventListener('click', () => {
     title.innerHTML= 'PRODUÇÃO' //Altere aqui o titulo (sempre dentro das 'aspas')
     subtitle.innerHTML= 'PRODUÇÃO BLA BLA BLA' //Altere aqui a descrição
     img.innerHTML = '<img src="images/alura.png" alt="">' //Altere aqui o caminho da imagem
+    //imagem só pra ver que tá funcionando
 })
 
 let title =  document.getElementById('title-botton')
 let subtitle =  document.getElementById('subtitle-botton')
 let img = document.getElementById('imge')
+
+
